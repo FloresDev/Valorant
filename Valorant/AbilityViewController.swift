79bf8424006d1f3a826f3a1578545a7b17dc21d0
Daniel <floresclifman@gmail.com>
2021-08-24 19:49:58 +0200

    Modified Gif, video abilities inside app

--- Valorant/AbilityViewController.swift
+++ Valorant/AbilityViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+// Importamos AVFoundation para trabajar con el vídeo (audio y vídeo)
+import AVFoundation
+
+//Para vídeo importamos AVKit para que tengamos acceso a los controles de reproducción
+import AVKit
+
 class AbilityViewController: UIViewController {
     
     @IBOutlet weak var iconImageView: UIImageView!
@@ -15,13 +21,15 @@ class AbilityViewController: UIViewController {
     
     @IBOutlet weak var abilityNameLabel: UILabel!
     @IBOutlet weak var abilitySlotLabel: UILabel!
-    
     @IBOutlet weak var abilityDescriptionLabel: UILabel!
+    @IBOutlet weak var video: UIView!
     
     var ability: Ability?
     var imagePerson: UIImage?
     var urlImagePerson: String?
     var connection = Connection()
+    // Reproductor para el vídeo (Proporcionado iOS)
+    var player : AVPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +45,40 @@ class AbilityViewController: UIViewController {
                 }
             }
         }
+        initPlayer()
        
+    }
+    
+    // Función para inicializar el player
+    func initPlayer() {
+        // Creamos la url a partir del Bundle ya que el archivo está dentro de la app
+        let url = Bundle.main.url(forResource: "SovaFlechas", withExtension: "mp4")
+        
+        // Seleccionamos la opción del constructor que recibe la url
+        player = AVPlayer(url: url!)
+        
+//        // Variable controller para la reproducción (Lo inicializamos vacío)
+//        let controller = AVPlayerViewController()
+//        // Al controlador hay que asociarle el reproductor
+//        controller.player = player
+//        // Presentamos el controlador para mostrarlo
+//        present(controller, animated: true)
+        
+        // Variable layer para el vídeo (Personalizado)
+        /*
+         Utilizamos esta variable para meter el reproductor
+         Esta variable es la capa que era antes controller en el proporcionado por iOS
+         */
+        let layer = AVPlayerLayer(player: player)
+        // Adaptamos el marco del vídeo a nuestra vista (video)
+        layer.frame = video.bounds
+        
+        // Añadimos el layer a la vista
+        video.layer.addSublayer(layer)
+        
+        
+        player.play()
+        
     }
     
 
