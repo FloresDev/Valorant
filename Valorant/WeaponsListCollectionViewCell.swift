538b6d270a302b4e961c8d4cb78ff11a1a652ba1
JorgeSuarez <Jorge@mail>
2021-08-25 17:09:47 +0200

    config WeaponsListView

--- Valorant/WeaponsListCollectionViewCell.swift
+++ Valorant/WeaponsListCollectionViewCell.swift
@@ -6,5 +6,6 @@ class WeaponsListCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var weaponImage: UIImageView!
     @IBOutlet weak var weaponName: UILabel!
+  
     
 }
--- Valorant/WeaponsListViewController.swift
+++ Valorant/WeaponsListViewController.swift
@@ -5,7 +5,6 @@ import UIKit
 class WeaponsListViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     @IBOutlet weak var collectionView: UICollectionView!
-   
     
     var connection = Connection()
     let spanishLanguage = "language=es-ES"
@@ -13,69 +12,50 @@ class WeaponsListViewController: UIViewController, UICollectionViewDelegate, UIC
     let MAX_IMAGE_DATA = 17
     var imageDonwlodaded = 0
     var loading = Loading()
-    var weapons2: [Weapon] = []
-    var num = 0
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
         collectionView.dataSource = self
-        
+
         connectionGetListAndImage()
         if imageDonwlodaded < MAX_IMAGE_DATA {
             self.loading.setupLoadingViews(controller: self)
             self.loading.showLoading(controller: self)
         
         }
-        
+      
 
         // Do any additional setup after loading the view.
     }
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return weapons2.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         
-        if let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header01", for: indexPath) as? HeaderWeaponsListCollectionReusableView{
-            sectionHeader.headerTitle01.text = "Rifles"
-            return sectionHeader
+            return weapons.count
+            
         }
-        return UICollectionReusableView()
         
-    }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+      
+     
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "weaponsCell", for: indexPath) as! WeaponsListCollectionViewCell
         
-        cell.weaponImage.image = weapons2[indexPath.row].image
-        cell.weaponName.text = weapons2[indexPath.row].displayName
-        cell.layer.borderWidth = 5
-        cell.layer.borderColor = #colorLiteral(red: 0.1379934549, green: 0.1608571708, blue: 0.1884222329, alpha: 1)
-        
-        
+        cell.weaponImage.image = weapons[indexPath.row].image
+        cell.weaponName.text = weapons[indexPath.row].displayName
+        cell.layer.cornerRadius = 10
+ 
         return cell
-    }
-    
-    
+
+        }
+
     func connectionGetListAndImage() {
         
         connection.getWeapons(language: spanishLanguage) { [self] (weapons) in
             if let weapons = weapons {
                 
-                for i in 0...weapons.data!.count - 1{
-                    if weapons.data![i].category == "EEquippableCategory::Rifle"{
-                        num = i
-                        
-                        for i in self.num...self.num{
-                            weapons2.append(weapons.data![i])
-                        }
-                        
-                    }
-                }
                 self.weapons = weapons.data!
                 
                 self.weapons = self.weapons.sorted { $0.displayName! < $1.displayName! }
@@ -97,6 +77,4 @@ class WeaponsListViewController: UIViewController, UICollectionViewDelegate, UIC
             }
         }
     }
-    
-
 }
