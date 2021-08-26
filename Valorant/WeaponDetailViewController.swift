ae59e80569a6ca0b410ee6c021f2663db15c0b71
JorgeSuarez <Jorge@mail>
2021-08-26 19:32:11 +0200

    fin bugs

---
+++ Valorant/WeaponDetailViewController.swift
@@ -0,0 +1,68 @@
+//
+//  WeaponDetailViewController.swift
+//  Valorant
+//
+//  Created by Jorge Suarez Calafat on 26/8/21.
+//
+
+import UIKit
+
+class WeaponDetailViewController: UIViewController {
+
+    @IBOutlet weak var weaponImage: UIImageView!
+    @IBOutlet weak var weponName: UILabel!
+
+    @IBOutlet weak var weaponName03: UILabel!
+  
+    @IBOutlet weak var weaponName06: UILabel!
+    @IBOutlet weak var weaponName07: UILabel!
+    @IBOutlet weak var weaponNamePrinc: UILabel!
+    @IBOutlet weak var weaponTypeLabel: UILabel!
+    
+    var connection = Connection()
+    var weaponuuid: String?
+    var loader = Loading()
+    let spanishLanguage = "language=es-ES"
+    var weapon: WeaponDow?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        connection.getWeapon(language: spanishLanguage, uuid: weaponuuid!) { [self] (weapon) in
+            if let weapon = weapon {
+               
+                DispatchQueue.main.async {
+                   
+                    
+                    weaponName03.text = weapon.data?.displayName
+                   
+                    weaponName06.text = weapon.data?.displayName
+                    weaponName07.text = weapon.data?.displayName
+                    weponName.text = weapon.data?.displayName
+                    weaponNamePrinc.text = weapon.data?.displayName
+                    weaponTypeLabel.text = weapon.data?.category
+                }
+
+                
+            
+            
+                connection.getImage(urlString: (weapon.data?.displayIcon)!) {[self] (image) in
+                if let image = image {
+                    DispatchQueue.main.async {
+                        weaponImage.image = image
+                      
+                    }
+
+                }
+    }
+              
+
+            }
+       
+           
+}
+        
+       
+}
+    
+}
